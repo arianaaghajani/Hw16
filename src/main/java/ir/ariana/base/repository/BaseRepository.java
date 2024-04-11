696eb0a0d9c57c8ac97c61dc9729af399ff53c7e
arianaaghajani <aghajaniariana@gmail.com>
2024-04-11 22:19:10 +0430

    add base repository interface and filed

---
+++ src/main/java/ir/ariana/base/repository/BaseRepository.java
@@ -0,0 +1,15 @@
+package ir.ariana.base.repository;
+
+import ir.ariana.base.entity.BaseEntity;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseRepository<T extends BaseEntity<ID>,ID extends Serializable>{
+
+    T saveOrUpdate(T entity);
+    Optional<T> findById(ID id);
+    void delete(T entity);
+    List<T> findAll();
+}
